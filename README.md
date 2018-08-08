7e0207cb5cc2c1ec06768936247c47e20c8557f6
serverlessguru <serverlessguru@gmail.com>
2018-08-07 22:34:11 -0700

    initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+## serverless-python-requirements plugin
+
+#### What?
+Code used in the video, COMING SOON
+
+#### Plugin
+https://github.com/UnitedIncome/serverless-python-requirements
---
+++ handler.py
@@ -0,0 +1,24 @@
+import json
+import requests
+
+def hello(event, context):
+    body = {
+        "message": "Python rocks!",
+        "input": event
+    }
+
+    response = {
+        "statusCode": 200,
+        "body": json.dumps(body)
+    }
+
+    return response
+
+    # Use this code if you don't use the http event with the LAMBDA-PROXY
+    # integration
+    """
+    return {
+        "message": "Go Serverless v1.0! Your function executed successfully!",
+        "event": event
+    }
+    """
